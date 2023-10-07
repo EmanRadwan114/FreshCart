f216bfcf07d915f51bd09854194f00022aab3bcd
EmanRadwan114 <emanalaa11499@gmail.com>
2023-10-07 08:23:12 +0300

    first commit

---
+++ src/Components/ForgetPass/ForgetPass.jsx
@@ -0,0 +1,73 @@
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import * as Yup from "yup";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
+export default function ForgetPass() {
+  let [error, setError] = useState(null);
+  let navigate = useNavigate();
+  let validationSchema = Yup.object({
+    email: Yup.string().email("email is invalid").required("email is required"),
+  });
+
+  let formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    validationSchema,
+    onSubmit: submitForgetPass,
+  });
+
+  async function submitForgetPass(values) {
+    let { data } = await axios
+      .post(
+        "https://ecommerce.routemisr.com/api/v1/auth/forgotPasswords",
+        values
+      )
+      .catch((err) => {
+        setError(err.response.data.message);
+      });
+    navigate("/resetCode");
+  }
+  return (
+    <main className="my-5 mx-auto w-75">
+      <Helmet>
+        <title>Forget Password</title>
+        <meta name="description" content="Forget Password Page" />
+      </Helmet>
+      {error ? (
+        <div className="alert alert-danger px-2 py-1 text-center">{error}</div>
+      ) : (
+        ""
+      )}
+
+      <h1 className="h4 text-main text-center fw-bold">Forget Password</h1>
+      <form method="post" onSubmit={formik.handleSubmit}>
+        <label htmlFor="email" className="fw-semibold">
+          Email:
+        </label>
+        <input
+          type="email"
+          name="email"
+          value={formik.values.email}
+          id="email"
+          className="form-control my-3"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.errors.email ? (
+          <div className="alert alert-danger px-2 py-1">
+            {formik.errors.email}
+          </div>
+        ) : (
+          ""
+        )}
+        <button type="submit" className="btn bg-main text-light">
+          Submit
+        </button>
+      </form>
+    </main>
+  );
+}
---
+++ src/Components/ResetCode/ResetCode.jsx
@@ -0,0 +1,81 @@
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import * as Yup from "yup";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
+export default function ResetCode() {
+  let [error, setError] = useState(null);
+  let navigate = useNavigate();
+  let validationSchema = Yup.object({
+    resetCode: Yup.string()
+      .matches(/^[0-9]{1,}$/, "Reset Code is invalid")
+      .required("Reset Code is required"),
+  });
+
+  let formik = useFormik({
+    initialValues: {
+      resetCode: "",
+    },
+    validationSchema,
+    onSubmit: submitResetCode,
+  });
+
+  async function submitResetCode(values) {
+    let { data } = await axios
+      .post(
+        "https://ecommerce.routemisr.com/api/v1/auth/verifyResetCode",
+        values
+      )
+      .catch((err) => {
+        setError(err.response.data.message);
+      });
+    navigate("/resetPassword");
+  }
+  return (
+    <main className="my-5 mx-auto w-75">
+      <Helmet>
+        <title>Reset Code</title>
+        <meta name="description" content="Reset Code Pages" />
+      </Helmet>
+      {error ? (
+        <div className="alert alert-danger px-2 py-1 text-center">{error}</div>
+      ) : (
+        ""
+      )}
+
+      <h1 className="h4 text-main text-center fw-bold mb-3">
+        Enter Reset Code
+      </h1>
+      <p className="text-center fs-5 fw-medium">
+        We've sent the reset code to your E-mail, please check it and enter the
+        code below.
+      </p>
+      <form method="post" onSubmit={formik.handleSubmit}>
+        <label htmlFor="resetCode" className="fw-semibold">
+          Reset Code:
+        </label>
+        <input
+          type="text"
+          name="resetCode"
+          value={formik.values.resetCode}
+          id="resetCode"
+          className="form-control my-3"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.errors.resetCode ? (
+          <div className="alert alert-danger px-2 py-1">
+            {formik.errors.resetCode}
+          </div>
+        ) : (
+          ""
+        )}
+        <button type="submit" className="btn bg-main text-light">
+          Submit
+        </button>
+      </form>
+    </main>
+  );
+}
